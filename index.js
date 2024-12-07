bba5347dc6bcbb71667be59f93276bfcf4a1b226
Mrthgamer <159038157+Mrthgamer@users.noreply.github.com>
2024-12-07 11:43:51 +0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["PLAY NOW", "MC.WORLDSHUB.ME"];
+const DescriptionMessages = ["PLAY NOW", "MC.WORLDSHUB.ME"];
 const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
